2f955e1ea4be15ee5943f4391491e0871fd5684c
jji3379 <jji3379@naver.com>
2023-01-18 00:07:21 +0900

    init commit

---
+++ src/main/java/com/example/barcoder/BarcoderApplication.java
@@ -0,0 +1,13 @@
+package com.example.barcoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarcoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarcoderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barcoder/BarcoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barcoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarcoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
